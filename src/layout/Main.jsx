43573f3c58ae073640a4c28d28aa32d847b19735
im-sanny <raselparvezsanny@gmail.com>
2024-11-17 19:02:24 +0600

    add: product component added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
 
 export default function Main() {
   return (
@@ -8,6 +9,7 @@ export default function Main() {
       <div>
         <Navbar />
         <Banner />
+        <Products />
         <Footer />
       </div>
     </>
