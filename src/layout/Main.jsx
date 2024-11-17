2f5160a827e3c2a4647ef2fdcdc83231558f6c69
im-sanny <raselparvezsanny@gmail.com>
2024-11-17 18:27:28 +0600

    add: navbar component added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,11 @@
+import Navbar from "../components/Navbar";
+
 export default function Main() {
   return (
     <>
-      <div>Main</div>
+      <div>
+        <Navbar/>
+      </div>
     </>
   );
 }
